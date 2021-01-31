1ff4b06791eb991ae4410f03b5f92d9a4ac43b05
helgs18 <helgs18@student.uia.no>
2021-01-31 21:44:18 +0100

    Adding SeekBar for timer

--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -1,22 +1,19 @@
 package no.uia.ikt205.pomodoro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
-    lateinit var thirtyMinuteButton:Button
-    lateinit var oneHourButton:Button
-    lateinit var ninetyMinutesButton:Button
-    lateinit var twoHoursButton:Button
     lateinit var coutdownDisplay:TextView
 
     var timeToCountDownInMs = 5000L
@@ -30,15 +27,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        thirtyMinuteButton = findViewById<Button>(R.id.thirtyMinutesButton)
-        oneHourButton = findViewById<Button>(R.id.oneHourButton)
-        ninetyMinutesButton = findViewById<Button>(R.id.ninetyMinutesButton)
-        twoHoursButton = findViewById<Button>(R.id.twoHoursButton)
+        /* Code for SeekBar
+        // Got help from:
+        // https://abhiandroid.com/ui/seekbar (using SeekBar)
+        // https://www.geeksforgeeks.org/seekbar-in-kotlin/ (progressChangedValue variable)
+        // ## The app crashed every time i tried to use the TimerFunction. I tried to use a cast
+        // such as setTimer(progressChangedValue as Long), and I think that is what caused probelms.
+        // So I created a new function, so I created the castIntToLong() function, that did the
+        // casting for me.*/
+        // ToDo: Try to find out how to use cast
+        val seek = findViewById<SeekBar>(R.id.seekBar)
+        seek?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            var progressChangedValue = 0
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                progressChangedValue = progress;
+            }
 
-        thirtyMinuteButton.setOnClickListener(){ setTimer(30*oneMinute) }
-        oneHourButton.setOnClickListener(){ setTimer(60*oneMinute) }
-        ninetyMinutesButton.setOnClickListener(){ setTimer(90*oneMinute) }
-        twoHoursButton.setOnClickListener(){ setTimer(120*oneMinute) }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Toast.makeText(this@MainActivity,
+                "Progress is: " + progressChangedValue + "%",
+                Toast.LENGTH_SHORT).show()
+                castIntToLong(progressChangedValue)
+                //setTimer(progressChangedValue)
+            }
+        })
+
 
         startButton = findViewById<Button>(R.id.startCountdownButton)
         startButton.setOnClickListener(){
@@ -56,9 +77,9 @@ class MainActivity : AppCompatActivity() {
     fun startCountDown(v: View){
         isTimerRunning = true
 
-        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+        timer = object : CountDownTimer(timeToCountDownInMs, timeTicks) {
             override fun onFinish() {
-                Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
                 isTimerRunning = false
             }
 
@@ -69,12 +90,17 @@ class MainActivity : AppCompatActivity() {
         timer.start()
     }
 
-    fun updateCountDownDisplay(timeInMs:Long){
+    fun updateCountDownDisplay(timeInMs: Long){
         coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
 
-    fun setTimer(timeInMs:Long){
+    fun setTimer(timeInMs: Long){
         timeToCountDownInMs = timeInMs
+    }
+
+    fun castIntToLong(myVar: Int){
+        var longVariable = 1L * myVar * oneMinute
+        setTimer(longVariable)
     }
 
 }
